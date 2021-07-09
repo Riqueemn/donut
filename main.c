9f1a16661d186594ceddbb031dd2fe840c797614
Henrique Moreira <henriquemoreira842@gmail.com>
2021-07-08 22:52:25 -0300

    adiconando movimentos de teclado

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 #include <math.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <conio.h>
+
+
 
 
 
@@ -14,7 +18,23 @@ int main() {
     float z[1760];
     char b[1760];
     printf("\x1b[2J");
+    int q = 0;
+    int ch;
     for(;;) {
+    	
+		if (kbhit()) {
+         ch = getch(); // Get typed character into ch
+         if ((int)ch == 27) //when esc button is pressed, then it will comeout from loop
+         break;
+      }
+      
+      if ((int)ch == 100) {
+      	q += 1;
+	  } else if ((int)ch == 97) {
+	  	q -= 1;
+	  }
+		
+		
         memset(b,32,1760);
         memset(z,0,7040);
         for(j=0; j < 6.28; j += 0.07) {
@@ -30,7 +50,7 @@ int main() {
                 float m = cos(B);
                 float n = sin(B);
                 float t = c * h * g - f * e;
-                int x = 40 + 30 * D * (l * h * m - t * n);
+                int x = 40 + q + 30 * D * (l * h * m - t * n);
                 int y= 12 + 15 * D * (l * h * n + t * m);
                 int o = x + 80 * y;
                 int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
